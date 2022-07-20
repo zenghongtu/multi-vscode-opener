6686a73ac5153d63ac09cd750b60604a5cedd2f5
zenghongtu <zenghongtu@gmail.com>
2022-07-20 12:12:17 +0800

    update deps

--- src/cli.ts
+++ src/cli.ts
@@ -3,14 +3,23 @@
 import commander from 'commander';
 import { startInteractiveRun, startRun } from './index';
 
-commander
-  .version(require('../package.json').version)
-  .arguments('[config name]')
-  .action((name: string) => {
-    if (name) {
-      startRun(name);
-    } else {
-      startInteractiveRun();
-    }
-  })
-  .parse(process.argv);
+const pkg = require('../package.json');
+
+const program = new commander.Command();
+
+program
+	.name('mvo')
+	.description(pkg.description)
+	.version(pkg.version, '-v, --version', 'output the current version')
+	.usage('[config-name]');
+
+program
+	.arguments('[config-name]')
+	.action((name: string) => {
+		if (name) {
+			startRun(name);
+		} else {
+			startInteractiveRun();
+		}
+	})
+	.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -7,95 +7,81 @@ import inquirer from 'inquirer';
 const noop = () => {};
 
 const getRootDirPath = (): string => {
-  const _dirName = '.' + require('../package.json').name;
-  return path.resolve(os.homedir(), _dirName);
+	const _dirName = '.' + require('../package.json').name;
+	return path.resolve(os.homedir(), _dirName);
 };
 
 const getDataPathsByName = (name: string): [string, string] => {
-  const _rootPath = getRootDirPath();
-  const _nameDirPath = path.join(_rootPath, name);
-  const extensionsDir = path.join(_nameDirPath, 'extensions');
-  const userDataDir = path.join(_nameDirPath, 'user-data');
-  return [extensionsDir, userDataDir];
+	const _rootPath = getRootDirPath();
+	const _nameDirPath = path.join(_rootPath, name);
+	const extensionsDir = path.join(_nameDirPath, 'extensions');
+	const userDataDir = path.join(_nameDirPath, 'user-data');
+	return [extensionsDir, userDataDir];
 };
 
 const getExecutable = (): string[] => {
-  switch (process.platform) {
-    case 'win32':
-      return [
-        os.homedir() + '/AppData/Local/Programs',
-        'c:/Program Files',
-        'c:/Program Files (x86)'
-      ].map(dir => path.join(dir, 'Microsoft VS Code', 'Code.exe'));
-    case 'darwin':
-      return [
-        path.join(
-          '/Applications',
-          'Visual Studio Code.app',
-          'Contents',
-          'MacOS',
-          'Electron'
-        )
-      ];
-    case 'linux':
-      return ['/usr/share/code'];
-    default:
-      throw new Error('Not supported platform!');
-  }
+	switch (process.platform) {
+		case 'win32':
+			return [os.homedir() + '/AppData/Local/Programs', 'c:/Program Files', 'c:/Program Files (x86)'].map((dir) =>
+				path.join(dir, 'Microsoft VS Code', 'Code.exe')
+			);
+		case 'darwin':
+			return [path.join('/Applications', 'Visual Studio Code.app', 'Contents', 'MacOS', 'Electron')];
+		case 'linux':
+			return ['/usr/share/code'];
+		default:
+			throw new Error('Not supported platform!');
+	}
 };
 
 export const startRun = (name: string) => {
-  console.log('Use config: ', name);
-  const [extensionsDir, userDataDir] = getDataPathsByName(name);
+	console.log('Use config: ', name);
+	const [extensionsDir, userDataDir] = getDataPathsByName(name);
+	console.log('extensionsDir: ', extensionsDir);
+	console.log('userDataDir: ', userDataDir);
 
-  const possibleExecutablePaths = getExecutable();
+	const possibleExecutablePaths = getExecutable();
 
-  let executable: string = '';
+	let executable: string = '';
 
-  try {
-    const _executable = possibleExecutablePaths.find(file => existsSync(file));
-    _executable && (executable = _executable);
-  } catch (e) {}
+	try {
+		const _executable = possibleExecutablePaths.find((file) => existsSync(file));
+		_executable && (executable = _executable);
+	} catch (e) {}
 
-  if (!executable) {
-    throw new Error(`Can't get executable for VS Code!`);
-  }
+	if (!executable) {
+		throw new Error(`Can't get executable for VS Code!`);
+	}
 
-  console.log('Executable: ', executable);
+	console.log('Executable: ', executable);
 
-  const sp = spawn(
-    `${executable}`,
-    [`--extensions-dir=${extensionsDir}`, `--user-data-dir=${userDataDir}`],
-    {
-      detached: true,
-      stdio: 'ignore'
-    }
-  );
-  sp.unref();
+	const sp = spawn(`${executable}`, [`--extensions-dir=${extensionsDir}`, `--user-data-dir=${userDataDir}`], {
+		detached: true,
+		stdio: 'ignore',
+	});
+	sp.unref();
 };
 
 export const startInteractiveRun = async () => {
-  let configs: { name: string }[];
+	let configs: { name: string }[];
 
-  try {
-    const dirents = readdirSync(getRootDirPath(), { withFileTypes: true });
-    configs = dirents
-      .filter(_dirent => _dirent.isDirectory())
-      .map(_dirent => ({ name: _dirent.name }));
-  } catch (e) {
-    configs = [];
-  }
+	try {
+		const dirents = readdirSync(getRootDirPath(), { withFileTypes: true });
+		configs = dirents.filter((_dirent) => _dirent.isDirectory()).map((_dirent) => ({ name: _dirent.name }));
+	} catch (e) {
+		configs = [];
+	}
 
-  if (configs.length < 1) {
-    configs.push({ name: 'default' });
-  }
+	if (configs.length < 1) {
+		configs.push({ name: 'default' });
+	}
 
-  const { name } = await inquirer.prompt<{ name: string }>({
-    name: 'name',
-    message: 'Select the config you want to use',
-    type: 'list',
-    choices: configs
-  });
+	const { name } = await inquirer.prompt<{ name: string }>({
+		name: 'name',
+		message: 'Select the config you want to use',
+		type: 'list',
+		choices: configs,
+	});
 
-  startRun(name);
+	startRun(name);
 };
